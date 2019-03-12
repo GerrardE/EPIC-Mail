6dac1aa0d1c32b096b860d22d432e4ec15a52cf2
GerrardE <ezeugwagerrard@gmail.com>
2019-03-12 17:13:51 +0100

    ch(UI): UI improvements - general UI improvements
    [Delivers #164493970]

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const indexRouter = express.Router();
-
-indexRouter.get('/', (req, res) => res.status(200).json({ message: 'Welcome to EPIC-Mail' }));
-
-indexRouter.all('*', (req, res) => res.status(404).json({ message: 'Page not found' }));
-
-export default indexRouter;
