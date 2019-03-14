4fc391a49cb5f00ea379130df2b1d1525963c879
GerrardE <ezeugwagerrard@gmail.com>
2019-03-14 09:21:08 +0100

    ch(api): improve backend api - write tests - modify controllers
    [Delivers #164576153]

---
+++ server/middlewares/mail.js
@@ -0,0 +1,105 @@
+import { users } from '../database/database';
+
+class MailValidatorHandler {
+  static createMailValidator(req, res, next) {
+    let {
+      // eslint-disable-next-line prefer-const
+      toEmail, subject, message
+    } = req.body;
+    
+    const foundUser = users.find(user => user.email === toEmail);
+    // Email Validation
+    if (toEmail === undefined) {
+      return res.status(400)
+        .json({
+          status: 400,
+          message: 'Error: email field cannot be empty',
+        });
+    }
+    toEmail = toEmail.toLowerCase().trim();
+    if (typeof toEmail !== 'string') {
+      return res.status(400)
+        .json({
+          status: 400,
+          message: 'Error: email should be a string'
+        });
+    }
+    if (toEmail === '') {
+      return res.status(400)
+        .json({
+          status: 400,
+          message: 'Error: email field cannot be empty.'
+        });
+    }
+    if (toEmail.includes(' ')) {
+      return res.status(400)
+        .json({
+          status: 400,
+          message: 'Error: email cannot include space.'
+        });
+    }
+    // email check: stackoverflow
+    const emailCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+    if (!emailCheck.test(toEmail)) {
+      return res.status(400).json({
+        status: 400,
+        error: 'Error: email format is invalid'
+      });
+    }
+    if (toEmail.length < 10 || toEmail.length > 30) {
+      return res.status(400).json({
+        status: 400,
+        error: 'Error: email should be 10 to 30 characters long'
+      });
+    }
+    
+    if (!foundUser) {
+      return res.status(400).json({
+        status: 400,
+        message: 'Error: email does not exist'
+      });
+    }
+
+    if (subject === undefined) {
+      return res.status(400)
+        .json({
+          status: 400,
+          message: 'Error: subject cannot be undefined',
+          sample: '{"subject": "string", "message": "string"}'
+        });
+    }
+    if (subject === '') {
+      return res.status(400)
+        .json({
+          status: 400,
+          message: 'Error: subject field cannot be empty',
+          sample: '{"subject": "string", "message": "string"}'
+        });
+    }
+    if (message === undefined) {
+      return res.status(400)
+        .json({
+          status: 400,
+          message: 'Error: message cannot be undefined',
+          sample: '{"subject": "string", "message": "string"}'
+        });
+    }
+    if (message === '') {
+      return res.status(400)
+        .json({
+          status: 400,
+          message: 'Error: message field cannot be empty',
+          sample: '{"subject": "string", "message": "string"}'
+        });
+    }
+
+    req.body.foundUser = foundUser;
+    req.body.subject = subject;
+    req.body.message = message;
+    return next();
+  }
+}
+
+const { createMailValidator } = MailValidatorHandler;
+
+export default createMailValidator;
--- server/tests/mockData/mockMessages.js
+++ server/tests/mockData/mockMessages.js
@@ -1,26 +1,43 @@
 const correctMessage = {
+  toEmail: 'johndoe@gmail.com',
   subject: 'First Subject',
   message: 'First Message'
 };
 
 const undefinedSubject = {
+  toEmail: 'johndoe@gmail.com',
   message: 'First Message'
 };
 
 const undefinedMessage = {
+  toEmail: 'johndoe@gmail.com',
   subject: 'First Subject'
 };
 
+const undefinedToEmail = {
+  subject: 'First Subject',
+  message: 'First Message'
+};
+
 const emptySubject = {
+  toEmail: 'johndoe@gmail.com',
   subject: '',
   message: 'First Message'
 };
 
 const emptyMessage = {
+  toEmail: 'johndoe@gmail.com',
   subject: 'First Subject',
   message: ''
 };
 
+const emptyToEmail = {
+  toEmail: '',
+  subject: 'First Subject',
+  message: 'First Message'
+};
+
 export {
-  correctMessage, undefinedMessage, undefinedSubject, emptyMessage, emptySubject 
+  correctMessage, undefinedMessage, undefinedSubject, emptyMessage, emptySubject, 
+  undefinedToEmail, emptyToEmail
 };
