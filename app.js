e5d5549fe8303a834e61fa2130bda5ce0a4a1ed0
GerrardE <ezeugwagerrard@gmail.com>
2019-04-05 15:20:19 +0100

    ch(api): Write tests - refactor test scripts
    [Delivers #164780877]

--- app.js
+++ app.js
@@ -11,28 +11,26 @@ import indexRouter from './server/routes/index';
 
 // Introduce the express middleware
 const app = express();
+dotenv.config();
+
+const corsOptions = {
+  origin: '*',
+  credentials: true
+};
+
+// Use the CORS
+app.use(cors(corsOptions));
 
 // Bring in the morgan middleware
 morgan('tiny');
 app.use(morgan('tiny'));
 
-dotenv.config();
-
-// const corsOptions = {
-//   origin: '*',
-//   credentials: true
-// };
-
-// Use the CORS
-app.use(cors());
 // Introduce the body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // declare your port constant
 const port = process.env.PORT || 3000;
-
-
 
 // Bring in the user route
 app.use(router);
